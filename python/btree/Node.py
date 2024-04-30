552736e29a9520b580611ba0e8db7fcb0751c026
Munashe Njanji <munashenjanji45@gmail.com>
2024-04-30 16:12:44 +0200

    Added the inserting capability of the Node

--- python/btree/Node.py
+++ python/btree/Node.py
@@ -22,3 +22,65 @@ class Node:
         self.keys: List[int] = []
         self.children: List[Node] = []
         self.leaf: bool = leaf
+
+    def insert(self, key: int):
+        """
+        Inserts a key into the node  or its child nodes recursively.
+
+        Args:
+        - key (int): The key to insert.
+        """
+        if self.leaf:
+            self._insert_leaf(key)
+        else:
+            index: int = len(self.keys - 1)
+            while index >= 0 and key < self.keys[index]:
+                index -= 1
+            index += 1
+            if len(self.children[index].keys) == 3:
+                self._split_child(index)
+                if key > self.keys[index]:
+                    index += 1
+            self.children[index].insert(key)
+
+    def _insert_leaf(self, key: int) -> None:
+        """
+        Inserts a key into the leaf node.
+
+        Args:
+        - key (int): The key to insert.
+        """
+        self.keys.append(key)
+        self.keys.sort()
+
+    def _split_child(self, index: int) -> None:
+        """
+        Splits a child node when it is full.
+
+        Args:
+        - index (int): The index of the child node to split.
+        """
+        new_child = Node(leaf=self.children[index].leaf)
+        split_index = len(self.children[index].keys) // 2
+        median = self.children[index].keys[split_index]
+
+        new_child.keys = self.children[index].keys[split_index + 1 :]
+        self.children[index].keys = self.children[index].keys[:split_index]
+
+        if not self.children[index].leaf:
+            new_child.children = self.children[index].children[split_index + 1 :]
+            self.children[index].children = self.children[index].children[
+                : split_index + 1
+            ]
+
+        self.keys.insert(index, median)
+        self.children.insert(index + 1, new_child)
+
+    def __str__(self) -> str:
+        """
+        Returns a string representation of the node.
+
+        Returns:
+        - str: A string representation of the node.
+        """
+        return f"{self.keys}"
